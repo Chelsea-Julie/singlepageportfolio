1560605cb27a5e817d197514b81d353e969040ee
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-06-26 11:44:24 +0200

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="lead">Chelsea Portfolio &copy;  <span class="shadow">{{ cYear }} </span></p>
+           
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: "FooterComp",
+    data() {
+        return {
+            cYear: new Date().getUTCFullYear()
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
