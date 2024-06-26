a0e50e9af17eb0b9d82e675f4fa833a76e32d6fd
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-06-26 12:32:57 +0200

    card Components

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card" >
+    <div class="card-header">
+        <slot name="cardHeader"></slot>
+  <div class="card-body">
+    <slot name="cardBody"></slot>
+    </div>
+  </div>
+</div>  
+</template>
+<script>
+export default {
+    name : "cardComp"
+    
+}
+</script>
+<style scoped >
+    .card{
+        width: 18rem;
+    }
+</style>
